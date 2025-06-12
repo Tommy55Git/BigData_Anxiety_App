bc0e4f99ad2c3beaf88d2874b239a8c99da26414
LeticiaLuis <160494864+LeticiaLuis@users.noreply.github.com>
2025-06-12 22:29:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -366,19 +366,7 @@ elif page == "Visualizations":
         fig3.update_layout(yaxis=dict(title='Nível Médio de Ansiedade'))
         st.plotly_chart(fig3, use_container_width=True)
 
-        # Scatter Plots com container exclusivo
-    psicologicas_cont = [
-        ("Stress Level (1-10)", "Nível de Estresse"),
-        ("Therapy Sessions (per month)", "Sessões de Terapia por Mês"),
-        ("Social Interaction Score", "Pontuação de Interação Social")
-    ]
-    
-    for col, label in psicologicas_cont:
-        if col in df.columns:
-            fig = px.scatter(df, x=col, y='Anxiety Level (1-10)',
-                             title=f"Ansiedade vs {label}")
-            with st.container():
-                st.plotly_chart(fig, use_container_width=True)
+
 
 
 
